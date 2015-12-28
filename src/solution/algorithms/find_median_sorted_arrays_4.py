3a8261a7de8e6549314d1fee0e793e7f6181cb8a
Larry Li <larrylee1213@gmail.com>
2015-12-29 02:15:15 +0800

    working on 4

--- src/solution/algorithms/find_median_sorted_arrays_4.py
+++ src/solution/algorithms/find_median_sorted_arrays_4.py
@@ -13,38 +13,47 @@ class Solution(object):
         lower_2, upper_2 = 0, len(nums2) - 1
 
         while True:
+            first_median = None
             p1, p2 = (lower_1 + upper_1) / 2, (lower_2 + upper_2) / 2
-            if nums1[p1] < nums2[p2]:
-                p1_at_most_index = p1 + p2
-                if (p2 == 0 or p1 == 0 or nums1[p1] > nums2[p2 - 1]) and p1_at_most_index == first_median_index:
-                    first_median = nums1[p1]
-                    if needs_second_median:
-                        second_median = nums1[p1 + 1] if (p1 + 1) < len(nums1) and nums1[p1 + 1] < nums2[p2] else nums2[p2]
-                        return (first_median + second_median) / 2.0
+
+            if p1 >= 0 and (p2 < 0 or nums1[p1] >= nums2[p2]):
+                if p2 + 1 == len(nums2) or nums1[p1] <= nums2[p2 + 1]:
+                    p1_final_index = p1 + p2 + 1
+                    if p1_final_index == first_median_index:
+                        first_median = nums1[p1]
                     else:
-                        return first_median
+                        if p1_final_index > first_median_index:
+                            upper_1 = p1 - 1
+                        else:
+                            lower_1 = p1
                 else:
-                    if p1_at_most_index < first_median_index:
-                        lower_1 = p1 + 1
-                    else:
-                        upper_2 = p2
+                    lower_2 = p2 + 1
             else:
-                p2_at_most_index = p1 + p2
-                if (p1 == 0 or p2 == 0 or nums2[p2] >= nums1[p1 - 1]) and p2_at_most_index == first_median_index:
-                    first_median = nums2[p2]
-                    if needs_second_median:
-                        second_median = nums2[p2 + 1] if (p2 + 1) < len(nums2) and nums2[p2 + 1] <= nums1[p1] else nums1[p1]
-                        return (first_median + second_median) / 2.0
+                if p1 + 1 == len(nums1) or nums1[p1 + 1] >= nums2[p2]:
+                    p2_final_index = p1 + p2 + 1
+                    if p2_final_index == first_median_index:
+                        first_median = nums2[p2]
                     else:
-                        return first_median
+                        if p2_final_index > first_median_index:
+                            upper_2 = p2 - 1
+                        else:
+                            lower_2 = p2 + 1
                 else:
-                    if p2_at_most_index < first_median_index:
-                        lower_2 = p2 + 1
+                    lower_1 = p1 + 1
+            if first_median is not None:
+                if needs_second_median:
+                    if p1 + 1 < len(nums1) and p2 + 1 < len(nums2):
+                        second_median = nums2[p2 + 1] if nums2[p2 + 1] > nums1[p1 + 1] else nums1[p1 + 1]
+                    elif p1 + 1 == len(nums1):
+                        second_median = nums2[p2 + 1]
                     else:
-                        upper_1 = p1
+                        second_median = nums1[p1 + 1]
+                    return (first_median + second_median) / 2.0
+                else:
+                    return first_median
 
 
 if __name__ == '__main__':
-    nums1 = [1,3]
-    nums2 = [1]
+    nums1 = [2]
+    nums2 = [1,3,4,5,6]
     print Solution().findMedianSortedArrays(nums1, nums2)
