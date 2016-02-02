823e054f53d03aa7912e4fd90f9fafecde6dc5b5
larryli <jingeli@ebay.com>
2016-02-02 14:50:21 +0800

    Finished al_005 but not efficient enough

--- src/solution/algorithms/al_005_longest_palindrome.py
+++ src/solution/algorithms/al_005_longest_palindrome.py
@@ -4,22 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        s_len = len(s)
         max_start, max_len = 0, 1
-        for i in range(0, len(s)):
-            j = len(s) - 1
-            while j > i:
-                j -= 1
-                if s[j] == s[i]:
-                    j_start_at = j
-                    j -= 1
-                    while j > i and s[j] == s[i + j_start_at - j]:
-                        j -= 1
-                    if j == i or j == i + 1:
-                        seq_len = j_start_at - i + 1
-                        if seq_len > max_len:
-                            max_start = i
-                            max_len = seq_len
+        for i in range(1, s_len * 2):
+            if s_len - i/2 <= max_len/2:
+                break
+            if i % 2 == 0:
+                left = i / 2 - 1
+                right = i / 2 + 1
+            else:
+                left = i / 2
+                right = i / 2 + 1
+            while left >= 0 and right < s_len and s[left] == s[right]:
+                left -= 1
+                right += 1
+            left += 1
+            right -= 1
+            current_len = right - left + 1
+            if current_len > max_len:
+                max_start = left
+                max_len = current_len
         return s[max_start: max_start + max_len]
+
 
 
 import unittest
@@ -27,5 +33,8 @@ import unittest
 
 class TestSolution(unittest.TestCase):
     def test_solution(self):
-        s = 'aaabaaaa'
-        self.assertEqual('aaabaaa', Solution().longestPalindrome(s))
+        # s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        s = "CCCC"
+        result = Solution().longestPalindrome(s)
+        # self.assertEqual("ABBA", result)
+        print result
