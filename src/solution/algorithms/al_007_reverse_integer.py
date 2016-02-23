cca864d60ddb92faf24b700eeb2151007c0b10c7
larryli <jingeli@ebay.com>
2016-02-23 15:24:38 +0800

    Correct min_int condition for al_007

--- src/solution/algorithms/al_007_reverse_integer.py
+++ src/solution/algorithms/al_007_reverse_integer.py
@@ -5,15 +5,17 @@ class Solution(object):
         :rtype: int
         """
         int_32_max = 2147483647
+        int_32_min = -int_32_max - 1
         positive = 1 if x >= 0 else -1
         x *= positive
         result = 0
         while x is not 0:
             digit = x % 10
-            if int_32_max - result * 10 >= digit:
-                result = result * 10 + digit
-            else:
-                return 0
+            if result != 0:
+                if int_32_max/result < 10 or int_32_max - result * 10 < digit \
+                        or int_32_min/-result < 10 or int_32_min + result * 10 > -digit:
+                    return 0
+            result = result * 10 + digit
             x /= 10
         return result * positive
 
@@ -22,5 +24,5 @@ import unittest
 
 class TestSolution(unittest.TestCase):
     def test_solution(self):
-        print Solution().reverse(-1004)
+        print Solution().reverse(-7463847412)
 
